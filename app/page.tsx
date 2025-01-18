22bc350aedf17e137fd61f9e5f44273f52c1e79f
TheoLambinet <theo.lamb@sfr.fr>
2025-01-18 11:38:29 +0100

    compétences faites !

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,21 @@ export default function Home() {
 
       <section className={styles.parcours}>
         <h2>Parcours</h2>
+
         <article>
           <h3>BAC</h3>
           <p>2018 - 2021</p>
           <p>NSI - Maths - SI</p>
           <p>Lycée Pierre Bayle, Sedan</p>
         </article>
+
         <article>
           <h3>BTS SIO</h3>
           <p>2021 - 2023</p>
           <p>Option SLAM</p>
           <p>Lycée Gaspard Monge, Charleville-Mézières</p>
         </article>
+
         <article>
           <h3>LP CRRW</h3>
           <p>2023 - 2024</p>
@@ -35,27 +38,77 @@ export default function Home() {
 
       <section className={styles.competences}>
         <h2>Compétences</h2>
+        <p>Les compétences que j'ai acquises au cours de mes études et expériences professionnelles :</p>
+
         <article>
-          <h3>Langages</h3>
-          <p>Java, Python, C, HTML/CSS, PHP, SQL</p>
+          <h3>C#</h3>
+          <img src="img/csharp.webp" alt="C#"/>
+          <p>Projets UWP, WPF</p>
+          <div>
+            <img src="img/dotnet.webp" alt=".NET"/>
+          </div>
         </article>
+        
+        <article>
+          <h3>Web</h3>
+          <img src="img/web.webp" alt="Web"/>
+          <p>HTML/CSS, JS, PHP</p>
+          <div>
+            <img src="img/html5.webp" alt="Html 5"/>
+            <img src="img/css.webp" alt="Css 3"/>
+            <img src="img/javascript.webp" alt="Js"/>
+            <img src="img/php.webp" alt="php"/>
+          </div>
+        </article>
+
+        <article>
+          <h3>Donnees</h3>
+          <img src="img/sql.webp" alt="SQL"/>
+          <p>SQL, MariaDB</p>
+          <div>
+            <img src="img/mariaDB.webp" alt="Maria DB"/>
+          </div>
+        </article>
+
+        <article>
+          <h3>ERP</h3>
+          <img src="img/xpp.webp" alt="Dynamics AX 2009"/>
+          <p>Développements sur AX</p>
+        </article>
+
         <article>
           <h3>Frameworks</h3>
-          <p>Spring, Django, Bootstrap</p>
+          <img src="img/react.webp" alt="React"/>
+          <p>NextJS, React Native</p>
+          <div>
+            <img src="img/nextjs.webp" alt="NextJS"/>
+            <img src="img/reactNative.webp" alt="React Native"/>
+          </div>
         </article>
+
         <article>
-          <h3>Outils</h3>
-          <p>Git, Docker, Jenkins, IntelliJ IDEA, Visual Studio Code</p>
+          <h3>Python</h3>
+          <img src="img/python.webp" alt="Python"/>
+          <p>Introduction avec Python</p>
+        </article>
+
+        <article>
+          <h3>Git</h3>
+          <img src="img/git.webp" alt="Git"/>
+          <p>Git pour les projets</p>
+        </article>
+
+        <article>
+          <h3>Power BI</h3>
+          <img src="img/powerBI.webp" alt="Power BI"/>
+          <p>Création de rapports</p>
         </article>
       </section>
       {/*
-      compétences / outils
       Expériences professionnelles
       pour la page projet : 
       menu dépliant par année
-      pour chaque projet, titre, descriptif, quelques images et les compétences */}
-      <p>Oui</p>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>e<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>d
+      pour chaque projet, titre, descriptif, alternant gauche droite, quelques images et les compétences */}
     </main>
   );
 }
